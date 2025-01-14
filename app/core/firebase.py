183d7176fd5b7a2bc088cc30195be80063c8bc09
purushothdl <purushothdl.aioverflow@gmail.com>
2025-01-14 10:13:22 +0530

    feat: Fix firebase key in config

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -2,13 +2,13 @@ import json
 import os
 import firebase_admin
 from firebase_admin import credentials, messaging
-from app.core.config import Settings
+from app.core.config import settings
 
 
 # Initialize Firebase
 def initialize_firebase():
     # Read the service account key JSON from environment variable
-    service_account_key_json = Settings.FIREBASE_SERVICE_ACCOUNT_KEY_JSON
+    service_account_key_json = settings.FIREBASE_SERVICE_ACCOUNT_KEY_JSON
 
     if service_account_key_json:
         # Parse the JSON content
